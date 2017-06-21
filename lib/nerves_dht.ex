8c7796a60de369610300c873a43953094d196126
Esdras Eduardo <esdrasedu@gmail.com>
2017-06-21 19:16:19 -0300

    Create projet

---
+++ lib/nerves_dht.ex
@@ -0,0 +1,18 @@
+defmodule NervesDht do
+  @moduledoc """
+  Documentation for NervesDht.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesDht.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_dht_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesDhtTest do
+  use ExUnit.Case
+  doctest NervesDht
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
