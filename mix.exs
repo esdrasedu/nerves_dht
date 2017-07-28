4a39eb8c24e30ce891b82e7ca837d2a2999816a0
Esdras Eduardo <esdrasedu@gmail.com>
2017-07-28 10:20:03 -0300

    update elixir

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NervesDht.Mixfile do
   def project do
     [app: :nerves_dht,
      version: "0.1.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.5",
      compilers: [:elixir_make] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
